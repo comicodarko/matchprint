1cecaae027e4bbbeda04168f69f6e40891bcfc22
Jeziel Marques <jezielmarques7@gmail.com>
2020-07-27 21:11:54 -0300

    contact page updated

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -2,11 +2,52 @@ import React from 'react';
 
 import NavBar from '../../components/NavBar';
 
+import {
+  Container,
+  Content,
+  Title,
+  DescriptionView,
+  TextDescription,
+  BadgeLogo,
+  BadgeText,
+  ContactTitle,
+  Buttons,
+} from './styled';
+
 export default function Contact() {
   return (
     <>
       <NavBar />
-      <h1>Hello Contact</h1>
-    </>    
+
+      <Container>
+        <Content>
+          <Title>MatchPrint</Title>
+
+          <DescriptionView>
+
+            <BadgeLogo>
+              <BadgeText>MP</BadgeText>
+            </BadgeLogo>
+
+            <TextDescription>
+              Nós somos uma empresa revolucionária em Recife que tem pronto
+              atendimento para país, nossos profissionais possuem mais de 30 anos
+              de experiência. Experiências que se somam para fornecer toda a qualidade
+              e segurança que você cliente merece. Superando as suas expectativas e
+              solucionando com inovação e dedicação a ação necessária para o sucesso
+              da sua empresa.
+            </TextDescription>
+          </DescriptionView>
+
+          <ContactTitle>Fale Conosco!</ContactTitle>
+
+          <Buttons>
+            <Title>MP</Title>
+            <Title>MP</Title>
+          </Buttons>
+
+        </Content>
+      </Container>
+    </>
   );
 }
--- src/pages/Contact/styled.js
+++ src/pages/Contact/styled.js
@@ -1,1 +1,59 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  margin-top: 50px;
+`;
+
+export const Title = styled.h1`
+  color: #fff;
+  font-size: 28px;
+  text-align: center;
+`;
+
+export const DescriptionView = styled.div`
+  width: 30%;
+  background: #fff2;
+  padding: 15px 25px;
+  border-radius: 20px;
+  margin: 20px 0 20px 33.5%;
+`;
+
+export const TextDescription = styled.p`
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+`;
+
+export const BadgeLogo = styled.div`
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background: #282a40;
+  margin: -32px 0 8px 5px;
+`;
+
+export const BadgeText = styled.p`
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+`;
+
+export const ContactTitle = styled.h1`
+  color: #fff;
+  font-size: 28px;
+  text-align: center;
+  margin-bottom: 25px;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
